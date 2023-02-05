bbe4e2ff5c6a6a381237e2597ea8bdfeac59cef5
unknown <dodamond222@gmail.com>
2023-02-05 21:45:27 +0900

    [2023-02-05] fix check id duplication

--- backend/trainer/src/main/java/mpti/common/exception/EmailDuplicateException.java
+++ backend/trainer/src/main/java/mpti/common/exception/EmailDuplicateException.java
@@ -1,5 +1,9 @@
 package mpti.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmailDuplicateException extends RuntimeException {
     public EmailDuplicateException(String email) {
         super("This email already exist : " + email);
